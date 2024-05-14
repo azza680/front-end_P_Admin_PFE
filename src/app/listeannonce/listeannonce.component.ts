067ba61f7928b6de1c9fe7cb4d9321b191a13c64
hazem lachheb <lachhebhazem87@gmail.com>
2024-05-14 02:10:30 +0100

    modification

--- src/app/listeannonce/listeannonce.component.ts
+++ src/app/listeannonce/listeannonce.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CrudService } from '../service/crud.service';
 import { Annonce } from '../Entites/Annonce.Entites';
 import { Utilisateur } from '../Entites/Utilisateur.Entites';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
 import { NgToastService } from 'ng-angular-popup';
 
 @Component({
@@ -20,20 +20,47 @@ export class ListeannonceComponent implements OnInit {
   nbannonce: number;
   id: number;
   collection: any[];
- 
+  annonceForm: FormGroup; 
+
   constructor(
     private fb: FormBuilder,
     private service: CrudService,
     private route: Router,
     private router: ActivatedRoute,
     private toast: NgToastService,
-  ) { }
-     
+  ) {
+    this.annonceForm = this.fb.group({ 
+      id: ['', Validators.required],
+      type_d_hebergement: ['', Validators.required],
+      nb_voyageur: ['', Validators.required],
+      nb_chamber: ['', Validators.required],
+      nb_lits: ['', Validators.required],
+      nb_salles: ['', Validators.required],
+      equipement: ['', Validators.required],
+      equipement_specail: ['', Validators.required],
+      equipement_securite: ['', Validators.required],
+      image: ['', Validators.required],
+      titre: ['', Validators.required],
+      description: ['', Validators.required],
+      reduction_semaine: [false],
+      reduction_mois: [false],
+      prix: ['', Validators.required],
+      pays: ['', Validators.required],
+      etat: [false],
+      ville: ['', Validators.required],
+      code_postale: ['', Validators.required],
+      heure_depart: ['', Validators.required],
+      heure_arriver: ['', Validators.required],
+      date: ['', Validators.required],
+      verification: [false]
+    });
+  }
+
   ngOnInit(): void {
     this.service.getAnnonce().subscribe((annonces: Annonce[]) => {
       this.listAnnonce = annonces;
     });
-
+   
     let idEvent = this.router.snapshot.params['id'];
     this.id = idEvent;
     console.log(this.id);
@@ -53,6 +80,7 @@ export class ListeannonceComponent implements OnInit {
       this.annonce = annonce;
       console.log("hatha id annonce :", annonce.id);
       console.log("hatha annonceur :", this.annonceur);
+      this.annonceForm.patchValue(annonce);
     });
   }
 
@@ -64,5 +92,9 @@ export class ListeannonceComponent implements OnInit {
         });
       });
     }
+  }
+
+  onSubmit(): void {
+    console.log(this.annonceForm.value);
   }
 }
